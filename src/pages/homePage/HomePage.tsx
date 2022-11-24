ef8d46d6d510ebbae029a1ca8af94808c69bc061
RahulGupta <micros2822@gmail.com>
2022-11-25 01:56:29 +0530

    added fonts

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,13 +1,14 @@
 import { Fragment } from "react";
 import ActivityInfoCard from "../../components/ActivityInfoCard/ActivityInfoCard";
-import ActivityInfoMenuButtons from "../../components/ActivityInfoMenuButtons/ActivityInfoMenuButtons";
+// import ActivityInfoMenuButtons from "../../components/ActivityInfoMenuButtons/ActivityInfoMenuButtons";
 import SearchBar from "../../components/SearchCard/Card";
 import { IHomePage } from "./HomePage.types";
+import styles from "./HomePage.module.css";
 
 const HomePage = ({ title = "Boredom is Qriosity" }: IHomePage) => {
   return (
     <Fragment>
-      <div>{title}</div>
+      <div className={styles.titleClass}>{title}</div>
       <SearchBar />
       <ActivityInfoCard headingText="dummy" briefText="dummy" CTAText="dummy" />
       {/* <ActivityInfoMenuButtons /> */}
